1c862a443c7822d824d576e1ad4812cbea93575b
ivanpr-dev <ivanprykhodkomail@gmail.com>
2025-10-15 10:57:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let myName = "Ivan";
+const myAge = 16;
+let isStudent = true;
+
+console.log(typeof myName); 
+console.log(typeof myAge); 
+console.log(typeof isStudent);
+
+let person = {
+    name: myName,
+    age: myAge
+};
+
+let hobbies = ["drawing", "coding", "chess"];
+
+console.log(person);
+console.log(hobbies);
